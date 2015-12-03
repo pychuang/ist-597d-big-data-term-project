1f9fa4f3fd3524a30e2bcfe921d9b21b406cffa2
Po-Yu Chuang <ratbert.chuang@gmail.com>
2015-12-02 23:02:00 -0500

    make all input lowercase

--- src/main/scala/Helper.scala
+++ src/main/scala/Helper.scala
@@ -12,7 +12,7 @@ object Helper extends Serializable {
   // Input:  ["I:1", "N:a1", "N:a2", "A:blah"]
   // Output: {"I": [1], "N": ["a1", "a2"], "A": ["blah"]}
   def rowToPaper(row: Array[String]): Map[String, Array[String]]= {
-      return row.groupBy(_(0).toString).map(x => (x._1, x._2.map(_.substring(2))))
+      return row.groupBy(_(0).toString).map(x => (x._1, x._2.map(_.substring(2)).map(_.toLowerCase)))
   }
 
   def summarize(papers: RDD[Map[String, Array[String]]], clustersOfPapers: RDD[Int]): Unit = {
